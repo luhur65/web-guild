8fd5fe6cd50ff41b2f0dac0cd6841f798962ad71
luhur65 <dharmabakti1202@gmail.com>
2010-12-20 02:31:14 +0700

    memberikan link view details

--- attr/User/home.php
+++ attr/User/home.php
@@ -1,6 +1,6 @@
 <div class="d-sm-flex align-items-center justify-content-between mb-4">
-    <h1 class="h3 mb-0 text-gray-800">Beranda Saya</h1>
-    <span class="text-muted small"><?= date('l , d M Y') ?></span>
+  <h1 class="h3 mb-0 text-gray-800">Beranda Saya</h1>
+  <span class="text-muted small"><?= date('l , d M Y') ?></span>
 </div>
 <hr class="bg-dark mb-3">
 
@@ -24,11 +24,11 @@ $id = $detail['guild_id'];
 
 <div class="mx-auto">
 
-    <?php foreach($queryPost as $p) :?>
+  <?php foreach($queryPost as $p) :?>
 
-    <?php if($p['id_guild'] === $p['guild_id']) : ?>
+  <?php if($p['id_guild'] === $p['guild_id']) : ?>
 
-    <?php 
+  <?php 
     
     // Menghitung Panjang Nama Usr
     $nama = explode(" ",$p['full_name']);
@@ -87,79 +87,95 @@ $id = $detail['guild_id'];
 
     ?>
 
-        <div class="mx-auto">
-              <div class="card shadow mb-4">
-                <!-- Card Header - Dropdown -->
-                <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
-                  <h6 class="m-0 font-weight-bold text-primary">
-                  <a href="<?= base_url; ?>/assets/img/user-icon/<?= $p['foto_profil']?>" class="example-image-link" data-lightbox="example-image" data-title="<?= $p['full_name'] ?>">
-                <img src="<?= base_url; ?>/assets/img/user-icon/<?= $p['foto_profil']; ?>" class="img-fluid img-anggota rounded-circle">
-                </a>
+  <div class="mx-auto">
+    <div class="card shadow mb-4">
+      <!-- Card Header - Dropdown -->
+      <div class="card-header py-3 d-flex flex-row align-items-center justify-content-between">
+        <h6 class="m-0 font-weight-bold text-primary">
+          <a href="<?= base_url; ?>/assets/img/user-icon/<?= $p['foto_profil']?>" class="example-image-link"
+            data-lightbox="example-image" data-title="<?= $p['full_name'] ?>">
+            <img src="<?= base_url; ?>/assets/img/user-icon/<?= $p['foto_profil']; ?>"
+              class="img-fluid img-anggota rounded-circle">
+          </a>
 
-            <?php if($panjangNama >= 3) : ?>
+          <?php if($panjangNama >= 3) : ?>
 
-            <?php if($p['id_user'] == $detail['id_user']) :?>
-                <a href="?mod=profile" class="h6"><?= $nama[0] . " " . $nama[1]; ?> ...</a>
-            <?php else : ?>
-                <a href="?mod=viewUser&q=<?= base64_encode($p['full_name'])?>" class="h6"><?= $nama[0] . " " . $nama[1]; ?> ...</a>
-            <?php endif ?>
+          <?php if($p['id_user'] == $detail['id_user']) :?>
+          <a href="?mod=profile" class="h6"><?= $nama[0] . " " . $nama[1]; ?> ...</a>
+          <?php else : ?>
+          <a href="?mod=viewUser&q=<?= base64_encode($p['full_name'])?>" class="h6"><?= $nama[0] . " " . $nama[1]; ?>
+            ...</a>
+          <?php endif ?>
 
           <?php elseif($panjangNama < 3) : ?>
-            
-            <?php if($p['id_user'] == $detail['id_user']) :?>
-                <a href="?mod=profile" class="h6"><?= $p['full_name']; ?></a>
-            <?php else : ?>
-                <a href="?mod=viewUser&q=<?= base64_encode($p['full_name']) ?>" class="h6"><?= $p['full_name'] ?></a>
-            <?php endif ?>
+
+          <?php if($p['id_user'] == $detail['id_user']) :?>
+          <a href="?mod=profile" class="h6"><?= $p['full_name']; ?></a>
+          <?php else : ?>
+          <a href="?mod=viewUser&q=<?= base64_encode($p['full_name']) ?>" class="h6"><?= $p['full_name'] ?></a>
+          <?php endif ?>
 
           <?php endif ?>
-          
-          &raquo;<a href="?mod=viewMyGuildID&idGuild=<?= base64_encode($p['id_guild']); ?>" class="mx-2 font-weight-bold"><?= $p['guild_name']; ?></a>
-          
-                  </h6>
-                  <?php if($p['id_user'] == $detail['id_user']) :?>
-                  <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-primary"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                    <a class="dropdown-item text-info" href="?mod=editMyPost&data=<?= base64_encode($p['id_post']) ?>"><i class="fas fa-edit fa-fw"></i> Edit</a>
-                  <a class="dropdown-item text-danger" href="?mod=hapusPostingan&data=<?= base64_encode($p['id_post']) ?>" onclick="return confirm('Anda Yakin ??')"><i class="fas fa-trash-alt fa-fw"></i> Hapus</a>
-                    </div>
-                  </div>
-                  <?php else : ?>
-                  <div class="dropdown no-arrow">
-                    <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-                      <i class="fas fa-ellipsis-v fa-sm fa-fw text-dark"></i>
-                    </a>
-                    <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
-                    <a class="dropdown-item text-info" href="?mod=viewUser&q=<?= base64_encode($p['full_name'])?>">
-                      <i class="fas fa-user fa-fw"></i> Lihat Profil
-                  </a>
-                  <a href="?mod=reportUser&data=<?= base64_encode($p['full_name']); ?>" class="dropdown-item text-danger"> <i class="fa fa-exclamation-triangle fa-fw" aria-hidden="true"></i> Laporkan Ini</a>
-                    </div>
-                  </div>
 
-                  <?php endif ?>
-                </div>
-                <!-- Card Body -->
-                <div class="card-body">
-                  <p class="small badge badge-info">
-                      <?= $p['full_name']; ?>
-                    </p>
-                  <span class="small badge badge-dark"> <?= $date[2] ." " . $bul ." ". $date[0]; ?>  </span>
-                  <p class="lead">
-                    <?php if(strlen($p['post']) > 900) :?>
-                      <?= substr($p['post'],0,900); ?> ... <a href="?mod=seeMore&data=<?= base64_encode($p['id_post']) ?>" class="btn btn-link">Lihat Selengkapnya</a>
-                      <?php else : ?>
-                      <?= $p['post']; ?>
-                      <?php endif ?>
-                  </p>
-                </div>
-              </div>
-            </div>
+          &raquo;<a href="?mod=viewMyGuildID&idGuild=<?= base64_encode($p['id_guild']); ?>"
+            class="mx-2 font-weight-bold"><?= $p['guild_name']; ?></a>
 
-      <?php endif ?>
+        </h6>
+        <?php if($p['id_user'] == $detail['id_user']) :?>
+        <div class="dropdown no-arrow">
+          <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-ellipsis-v fa-sm fa-fw text-primary"></i>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item text-info" href="?mod=editMyPost&data=<?= base64_encode($p['id_post']) ?>"><i
+                class="fas fa-edit fa-fw"></i> Edit</a>
+            <a class="dropdown-item text-danger" href="?mod=hapusPostingan&data=<?= base64_encode($p['id_post']) ?>"
+              onclick="return confirm('Anda Yakin ??')"><i class="fas fa-trash-alt fa-fw"></i> Hapus</a>
+          </div>
+        </div>
+        <?php else : ?>
+        <div class="dropdown no-arrow">
+          <a class="dropdown-toggle" href="#" role="button" id="dropdownMenuLink" data-toggle="dropdown"
+            aria-haspopup="true" aria-expanded="false">
+            <i class="fas fa-ellipsis-v fa-sm fa-fw text-dark"></i>
+          </a>
+          <div class="dropdown-menu dropdown-menu-right shadow animated--fade-in" aria-labelledby="dropdownMenuLink">
+            <a class="dropdown-item text-info" href="?mod=viewUser&q=<?= base64_encode($p['full_name'])?>">
+              <i class="fas fa-user fa-fw"></i> Lihat Profil
+            </a>
+            <a href="?mod=reportUser&data=<?= base64_encode($p['full_name']); ?>" class="dropdown-item text-danger"> <i
+                class="fa fa-exclamation-triangle fa-fw" aria-hidden="true"></i> Laporkan Ini</a>
+          </div>
+        </div>
 
-    <?php endforeach ?>
+        <?php endif ?>
+      </div>
+      <!-- Card Body -->
+      <div class="card-body">
+        <a href="?mod=seeMore&data=<?= base64_encode($p['id_post']) ?>" class="text-dark">
+        <p class="small badge badge-info">
+          <?= $p['full_name']; ?>
+        </p>
+        <span class="small badge badge-dark"> <?= $date[2] ." " . $bul ." ". $date[0]; ?> </span>
+          <p class="lead">
+            <?php if(strlen($p['post']) > 900) :?>
+            <?= substr($p['post'],0,900); ?> ... <a href="?mod=seeMore&data=<?= base64_encode($p['id_post']) ?>"
+              class="btn btn-link">Lihat Selengkapnya</a>
+            <?php else : ?>
+            <?= $p['post']; ?>
+            <?php endif ?>
+          </p>
+        <!-- Tombol like , comment -->
+        <a href="" class="btn btn-link btn-sm"><i class="fa fa-thumbs-up fa-fw" aria-hidden="true"></i> Like</a>
+        <a href="" class="btn btn-link btn-sm"><i class="fa fa-comments fa-fw" aria-hidden="true"></i> Comment</a>
+
+        </a>
+      </div>
+    </div>
+  </div>
+
+  <?php endif ?>
+
+  <?php endforeach ?>
 </div>
