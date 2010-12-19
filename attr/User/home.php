bca18bf2b496cf575065179c716838e59f8f2802
luhur65 <dharmabakti1202@gmail.com>
2010-12-20 02:09:27 +0700

    membuat single img di bagian home , lightbox

--- attr/User/home.php
+++ attr/User/home.php
@@ -93,7 +93,7 @@ $id = $detail['guild_id'];
       <div class="card-header border-0 bg-white py-3 d-flex flex-row align-items-center justify-content-between mb-0">
         <h6 class="m-0 font-weight-bold text-primary">
           <a href="<?= base_url; ?>/assets/img/user-icon/<?= $p['foto_profil']?>" class="example-image-link"
-            data-lightbox="example-image" data-title="<?= $p['full_name'] ?>">
+            data-lightbox="<?= $p['foto_profil']?>" data-title="<?= $p['full_name'] ?>">
             <img src="<?= base_url; ?>/assets/img/user-icon/<?= $p['foto_profil']; ?>"
               class="img-fluid img-anggota rounded-circle">
           </a>
