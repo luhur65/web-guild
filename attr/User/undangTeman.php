b98bc171084cb0eba1ebdefacea1e8e8a8dea044
luhur65 <dharmabakti1202@gmail.com>
2010-12-20 00:15:32 +0700

    project selesai

--- attr/User/undangTeman.php
+++ attr/User/undangTeman.php
@@ -17,40 +17,43 @@ if (isset($_POST['ok'])) {
 </div>
 <hr class="bg-secondary">
 
-<form action="" method="post">
-    <div class="row">
-        <div class="col-lg">
-            <div class="form-group">
+<div class="row">
+    <div class="col-lg-6">
+        <form action="" method="post">
+            <div class="input-group">
                 <input type="text" name="search" class="form-control" placeholder="Type name ... " required>
+                <div class="input-group-append">
+                    <button class="btn btn-primary" type="submit" name="ok">
+                        <i class="fa fa-search fa-fw" aria-hidden="true"></i>
+                    </button>
+                </div>
             </div>
+        
+        </form>
 
-        </div>
-
-        <div class="col-lg-3">
-            <button class="btn btn-outline-primary btn-block" type="submit" name="ok">
-                <i class="fa fa-search fa-fw" aria-hidden="true"></i> Search Friends
-            </button>
-        </div>
     </div>
-</form>
+</div>
+
 
 <!-- List Teman  -->
 <div class="row mx-auto">
-<?php if(isset($_POST['ok'])) :?>
-<?php foreach($friends as $f) : ?>
-    <div class="col-lg-8 card mt-3 p-2 <?= $retVal = ($f['role'] == 1) ? 'border-left-primary' : 'border-left-success' ; ?>">
-        <a class="dropdown-item d-flex align-items-center justify-content-start" href="?mod=invite&q=<?= base64_encode($f['id_user'])?>">
+    <?php if(isset($_POST['ok'])) :?>
+    <?php foreach($friends as $f) : ?>
+    <div
+        class="col-lg-8 card mt-3 p-2 <?= $retVal = ($f['role'] == 1) ? 'border-left-primary' : 'border-left-success' ; ?>">
+        <a class="dropdown-item d-flex align-items-center justify-content-start"
+            href="?mod=invite&q=<?= base64_encode($f['id_user'])?>">
             <div class="dropdown-list-image my-2">
                 <img class="rounded-circle img-thumbnail"
                     src="<?= base_url; ?>/assets/img/user-icon/<?= $f['foto_profil']; ?>" alt="">
             </div>
             <div class="font-weight-bold col-lg-7">
-                <div class="text-truncate"><?= $f['full_name']; ?> 
-            </div>
-            <div class="small text-gray-500 text-truncate"><?= $f['biografi']; ?></div>
+                <div class="text-truncate"><?= $f['full_name']; ?>
+                </div>
+                <div class="small text-gray-500 text-truncate"><?= $f['biografi']; ?></div>
             </div>
         </a>
     </div>
-<?php endforeach ?>
-<?php endif ?>
+    <?php endforeach ?>
+    <?php endif ?>
 </div>
