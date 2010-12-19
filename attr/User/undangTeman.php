6e4515df22cca4a6e9ce852e547a1c4d66a64645
luhur65 <dharmabakti1202@gmail.com>
2010-12-20 03:48:13 +0700

    ubah tampilan undangTeman.php

--- attr/User/undangTeman.php
+++ attr/User/undangTeman.php
@@ -1,13 +1,11 @@
 <?php 
 
 // Data Teman 
-
 $friends = "SELECT * FROM guild_info_member";
 
 if (isset($_POST['ok'])) {
 
     $search = htmlspecialchars(addslashes(strip_tags($_POST['search'])),ENT_QUOTES);
-    
     $friends = inviteFriend($search);
 }
 
@@ -20,25 +18,39 @@ if (isset($_POST['ok'])) {
 <hr class="bg-secondary">
 
 <form action="" method="post">
-    <div class="form-group col-lg-5 form-inline">
-        <input type="text" name="search" class="form-control mr-2">
-        <button class="btn btn-outline-primary" type="submit" name="ok">
-            <i class="fa fa-search-plus fa-fw" aria-hidden="true"></i>
-        </button>
+    <div class="row">
+        <div class="col-lg">
+            <div class="form-group">
+                <input type="text" name="search" class="form-control" placeholder="Type name ... " required>
+            </div>
+
+        </div>
+
+        <div class="col-lg-3">
+            <button class="btn btn-outline-primary btn-block" type="submit" name="ok">
+                <i class="fa fa-search fa-fw" aria-hidden="true"></i> Search Friends
+            </button>
+        </div>
     </div>
 </form>
 
 <!-- List Teman  -->
+<div class="row mx-auto">
 <?php if(isset($_POST['ok'])) :?>
-    <?php foreach($friends as $f) : ?>
-    <a class="dropdown-item d-flex align-items-center col-lg-5" href="?mod=invite&q=<?= base64_encode($f['id_user'])?>">
-        <div class="dropdown-list-image mr-3">
-            <img class="rounded-circle img-thumbnail" src="<?= base_url; ?>/assets/img/user-icon/<?= $f['foto_profil']; ?>" alt="">
-        </div>
-        <div class="font-weight-bold col-lg-7">
-            <div class="text-truncate"><?= $f['full_name']; ?></div>
-            <div class="small text-gray-500 text-truncate">Bio :: <?= $f['biografi']; ?></div>
-        </div>
-    </a>
-    <?php endforeach ?>
+<?php foreach($friends as $f) : ?>
+    <div class="col-lg-8 card mt-3 p-2 <?= $retVal = ($f['role'] == 1) ? 'border-left-primary' : 'border-left-success' ; ?>">
+        <a class="dropdown-item d-flex align-items-center justify-content-start" href="?mod=invite&q=<?= base64_encode($f['id_user'])?>">
+            <div class="dropdown-list-image my-2">
+                <img class="rounded-circle img-thumbnail"
+                    src="<?= base_url; ?>/assets/img/user-icon/<?= $f['foto_profil']; ?>" alt="">
+            </div>
+            <div class="font-weight-bold col-lg-7">
+                <div class="text-truncate"><?= $f['full_name']; ?> 
+            </div>
+            <div class="small text-gray-500 text-truncate"><?= $f['biografi']; ?></div>
+            </div>
+        </a>
+    </div>
+<?php endforeach ?>
 <?php endif ?>
+</div>
