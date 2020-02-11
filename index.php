633a1ef533900abea2956d98b80ab4da077d4dff
luhur65 <dharmabakti1202@gmail.com>
2020-02-11 13:26:00 +0700

    Membuat Readme baru

--- index.php
+++ index.php
@@ -1,22 +1,21 @@
 <?php
 
-require_once 'assets/bg-img/bg-masthead.jpg'
+
+require_once 'assets/bg-img/bg-masthead.jpg';
 
 
 /**
  * Configurasi Database :
  * Buka folder (config) ==> config.php
- * 
+ *
  * Halaman Admin
  * Buka Folder (attr) ==> Folder (Admin)
- * 
+ *
  * Halaman User
  * Buka Folder (attr) ==> Folder (User)
- * 
+ *
  * Halaman Template Web
  * Buka Folder (attr) ==> Folder (temp)
- * 
+ *
  * Sekian & TerimaKasih...........
  */
-
-?>
